073800764bcb7c801fd8286e1dd89c9daea721f5
Kenro <kenro2002@gmail.com>
2023-07-18 17:55:30 +0000

    moved the rest of description handling into jira-description module

--- lua/jira-description.lua
+++ lua/jira-description.lua
@@ -1,19 +1,51 @@
 M = {}
-M.write_desc_text = function(buf, paragraph_row, col, desc)
+M.write_desc_text = function(buf, paragraph_row, col, desc, hl_ns)
   vim.api.nvim_buf_set_text(buf, paragraph_row, col, paragraph_row, col, { desc.text })
   if desc.marks ~= nil then
     for _, mark in ipairs(desc.marks) do
       if mark.type == "link" then
-        vim.api.nvim_buf_add_highlight(
-          buf,
-          M.namespace,
-          "String",
-          paragraph_row - 1,
-          col,
-          col + string.len(desc.text)
-        )
+        vim.api.nvim_buf_add_highlight(buf, hl_ns, "String", paragraph_row, col, col + string.len(desc.text))
       end
     end
   end
+end
+M.write_emoji = function(buf, paragraph_row, col, inner_content)
+  vim.api.nvim_buf_set_text(buf, paragraph_row, col, paragraph_row, col, { inner_content.attrs.text })
+end
+M.write_mention = function(buf, paragraph_row, col, inner_content, hl_ns)
+  vim.api.nvim_buf_set_text(buf, paragraph_row, col, paragraph_row, col, { inner_content.attrs.text })
+  print("column=", col, col + string.len(inner_content.attrs.text))
+  vim.api.nvim_buf_add_highlight(buf, hl_ns, "String", paragraph_row, col, col + string.len(inner_content.attrs.text))
+end
+M.write_description_to_buf = function(buf, row, description, namespace)
+  local lines = {}
+  local row_offset = 0
+  if description then
+    for _, outer_content in ipairs(description.content) do
+      if outer_content.type == "paragraph" then
+        local paragraph_row = row + row_offset
+        vim.api.nvim_buf_set_lines(buf, paragraph_row, paragraph_row, false, { "" })
+        row_offset = row_offset + 1
+        local col = 0
+        for _, inner_content in ipairs(outer_content.content) do
+          if inner_content.type == "text" then
+            M.write_desc_text(buf, paragraph_row, col, inner_content, namespace)
+            col = col + string.len(inner_content.text)
+          end
+          if inner_content.type == "emoji" then
+            M.write_emoji(buf, paragraph_row, col, inner_content)
+            col = col + string.len(inner_content.attrs.text)
+          end
+          if inner_content.type == "mention" then
+            M.write_mention(buf, paragraph_row, col, inner_content, namespace)
+            col = col + string.len(inner_content.attrs.text)
+          end
+        end
+      end
+    end
+  else
+    table.insert(lines, "No description")
+  end
+  return lines
 end
 return M
--- lua/jira-nvim.lua
+++ lua/jira-nvim.lua
@@ -1,5 +1,6 @@
 local jira_api = require("jira-api")
 local buf_handle = require("buffer")
+local jira_description_handler = require("jira-description")
 local M = {}
 -- [
 --    Issues : {
@@ -66,59 +67,6 @@ M.render = function(buf)
 		vim.api.nvim_buf_set_extmark(buf, M.namespace, 0, 0, { end_row = 1, hl_group = "Title" })
 	end
 end
-M.write_description_to_buf = function(buf, row, description)
-	local lines = {}
-	local row_offset = 0
-	if description then
-		for _, outer_content in ipairs(description.content) do
-			if outer_content.type == "paragraph" then
-				local paragraph_row = row + row_offset
-				vim.api.nvim_buf_set_lines(buf, paragraph_row, paragraph_row, false, { "" })
-				row_offset = row_offset + 1
-				local col = 0
-				for _, inner_content in ipairs(outer_content.content) do
-					if inner_content.type == "text" then
-						require("jira-description").write_desc_text(buf, paragraph_row, col, inner_content)
-						col = col + string.len(inner_content.text)
-					end
-					if inner_content.type == "emoji" then
-						vim.api.nvim_buf_set_text(
-							buf,
-							paragraph_row,
-							col,
-							paragraph_row,
-							col,
-							{ inner_content.attrs.text }
-						)
-						col = col + string.len(inner_content.attrs.text)
-					end
-					if inner_content.type == "mention" then
-						vim.api.nvim_buf_set_text(
-							buf,
-							paragraph_row,
-							col,
-							paragraph_row,
-							col,
-							{ inner_content.attrs.text }
-						)
-						vim.api.nvim_buf_add_highlight(
-							buf,
-							M.namespace,
-							"String",
-							row + row_offset - 1,
-							col,
-							col + string.len(inner_content.attrs.text)
-						)
-						col = col + string.len(inner_content.attrs.text)
-					end
-				end
-			end
-		end
-	else
-		table.insert(lines, "No description")
-	end
-	return lines
-end
 M.expand = function()
 	local win = vim.api.nvim_get_current_win()
 	local cursor_pos = vim.api.nvim_win_get_cursor(win)
@@ -128,7 +76,12 @@ M.expand = function()
 	local issue_key = string.sub(line, 0, range_end - 1)
 	local buffer_text = {}
 	if M.buf[buf].issues[issue_key].expanded == false then
-		M.write_description_to_buf(buf, cursor_pos[1], M.buf[buf].issues[issue_key].fields.description)
+		jira_description_handler.write_description_to_buf(
+			buf,
+			cursor_pos[1],
+			M.buf[buf].issues[issue_key].fields.description,
+			M.namespace
+		)
 	end
 end
 M.close = function()
