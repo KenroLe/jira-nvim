958e2b5fd129615ba7885047cffe872567017fd9
Kenro <kenro2002@gmail.com>
2023-07-18 17:30:22 +0000

    use a different query

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,5 +1,5 @@
 curl --request GET \
-  --url $jira_url'/rest/api/3/search?jql=key%20%3D%20"UCON-556"' \
+  --url $jira_url'/rest/api/3/search?jql=key%20%3D%20"TES-4"' \
   --user $jira_email':'$jira_api_token \
   --header 'Accept: application/json' \
   --output '/tmp/curl-out.json'
