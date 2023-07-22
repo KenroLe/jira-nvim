e8f1b120459a6221610e45e5557acc276650e61b
Kenro <kenro2002@gmail.com>
2023-07-22 13:17:51 +0000

    more generic query.sh

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,5 +1,5 @@
 curl --request GET \
-  --url $jira_url'/rest/api/3/search?jql=key%20%3D%20"TES-4"' \
+  --url $jira_url'/rest/api/3/search?fields=summary,description&jql=text~%20test%20ORDER%20BY%20created%20DESC&startIndex=50' \
   --user $jira_email':'$jira_api_token \
   --header 'Accept: application/json' \
   --output '/tmp/curl-out.json'
